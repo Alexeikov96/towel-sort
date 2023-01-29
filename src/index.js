2f9f43ad01f4baa2e0227b2fbed5bc5afe2048e0
alexeikov96 <speedfaer96@gmail.com>
2023-01-29 04:32:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ module.exports = function towelSort (matrix) {
   if (!matrix || matrix[0] == null) {
     return []
   } else {
-    for (i = 1; i <= matrix.length; i += 2) {
-      matrix[i] = matrix[i].reverse()
+    for (i = 1; i < matrix.length; i += 2) {
+      matrix[i].reverse()
     }
     return matrix.join().split(',')
-  }
-  
+  }  
 }
 
-/* const matrix1 = [];
-console.log(towelSort(matrix1)) */
