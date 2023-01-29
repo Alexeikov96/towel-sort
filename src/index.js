e50c5f14016f21c817e90306392da93e00f715ed
alexeikov96 <speedfaer96@gmail.com>
2023-01-29 03:06:05 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function towelSort (matrix) {
   for (i = 1; i <= matrix.length; i += 2) {
-    matrix[i] = matrix[i].reverse
+    matrix[i] = matrix[i].reverse()
   }
   return matrix.join(',')
 }
